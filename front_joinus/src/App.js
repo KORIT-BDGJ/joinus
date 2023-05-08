8312d545b04e3bb2f237fb10db12e3dea74d07d5
jobc90 <jobc90@gmail.com>
2023-05-08 09:50:54 +0900

    기본 화면 9개 구성완료 후 최초 통합Merge완료

--- front_joinus/src/App.js
+++ front_joinus/src/App.js
@@ -3,10 +3,14 @@ import { Global } from '@emotion/react';
 import { Reset } from './Styles/Global/reset';
 import Login from './pages/Login/Login';
 import UserRegister from './pages/UserRegister/UserRegister';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import UserInfo from './pages/UserInfo/UserInfo';
 import Main from './pages/Main/Main';
 import PostRegister from './pages/Post/PostRegister/PostRegister';
+import OwnerPostList from './pages/Post/PostList/OwnerPostList';
+import HostPostList from './pages/Post/PostList/HostPostList';
+import OwnerPostDetail from './pages/Post/PostDetail/OwnerPostDetail';
+import HostPostDetail from './pages/Post/PostDetail/HostPostDetail';
 
 function App() {
   return (
@@ -18,7 +22,10 @@ function App() {
         <Route path="/userinfo" element={<UserInfo />} />
         <Route path="/main" element={<Main />} />
         <Route path="/postregister" element={<PostRegister />} />
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/ownerpostlist" element={<OwnerPostList />} />
+        <Route path="/hostpostlist" element={<HostPostList />} />
+        <Route path="/ownerpostdetail" element={<OwnerPostDetail />} />
+        <Route path="/hostpostdetail" element={<HostPostDetail />} />
       </Routes>
     </>
   );
