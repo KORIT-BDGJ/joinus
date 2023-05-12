423c7205498b91e996fe70cfbc6f7cd159060fdd
leesfact <lky110408@gmail.com>
2023-05-12 16:53:05 +0900

    23.05.12 feat : 회원가입 구현 완료

--- back_joinus/src/main/java/com/portfolio/joinus/joinus/controller/advice/AdviceController.java
+++ back_joinus/src/main/java/com/portfolio/joinus/joinus/controller/advice/AdviceController.java
@@ -1,7 +1,6 @@
 package com.portfolio.joinus.joinus.controller.advice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,8 +17,8 @@ public class AdviceController {
 		return ResponseEntity.badRequest().body(new ErrorResponseDto<>(e.getMessage(), e.getErrorMap()));
 	}
 	
-	@ExceptionHandler(UsernameNotFoundException.class)
-	public ResponseEntity<?> customException(UsernameNotFoundException e) {
-		return ResponseEntity.badRequest().body(new ErrorResponseDto<>(e.getMessage(), null));
-	}
+//	@ExceptionHandler(UsernameNotFoundException.class)
+//	public ResponseEntity<?> customException(UsernameNotFoundException e) {
+//		return ResponseEntity.badRequest().body(new ErrorResponseDto<>(e.getMessage(), null));
+//	}
 }
